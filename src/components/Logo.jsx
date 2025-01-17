4b85e04b2f5b3ffa9cf88f8a4f0ffcee53f71d7a
unknown <sunebarikara@gmail.com>
2025-01-17 12:45:10 +0100

    changed the new Navbar style

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Logo = () => {
     return (
-        <div className="ml-3 text-xl ">
+        <div className="flex justify-center items-center text-xl " style={{ whiteSpace: 'nowrap'}}>
             Video Script Generator
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,12 @@ const Navbar = () => {
     <>
       <header className="text-gray-600 body-font px-[100px]">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <div className="font-medium text-gray-900 mb-4 ">
             <Link to={"/"}>
               <Logo />
             </Link>
           </div>
-          <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <nav className=" flex items-center text-base justify-center">
             <div><Link to="/about" className="mr-5 hover:text-gray-900 cursor-pointer">About</Link></div>
             <div><Link to="/content" className="mr-5 hover:text-gray-900 cursor-pointer">Content</Link></div>
             <div><Link to="/product" className="mr-5 hover:text-gray-900 cursor-pointer">Product</Link></div>
